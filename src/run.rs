46a35861eb59200d454e203b2984a12284c3b6d6
qropa <124854119+qropa@users.noreply.github.com>
2024-12-10 21:04:10 +0900

    a

--- src/run.rs
+++ src/run.rs
@@ -1,8 +1,13 @@
+use core::fmt;
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use crate::settings;
 
 use anyhow::Result;
 use clap::Parser;
+use colored::Colorize;
 use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
 
 #[derive(Parser)]
 pub struct RunArgs {
@@ -30,32 +35,99 @@ fn parse_seed(s: &str) -> Result<(u32, u32)> {
 }
 
 pub fn run(args: RunArgs) -> Result<()> {
-    let _config = settings::read_settings()?;
-    let input = (0..100)
-        .map(|_| (rnd::get(100), rnd::get(100)))
-        .collect::<Vec<_>>();
-
     let seed = args.seed;
     let start = seed.0;
     let end = seed.1;
 
-    let pool = rayon::ThreadPoolBuilder::new().num_threads(4).build()?;
+    let _config = settings::read_settings()?;
+
+    let pool = rayon::ThreadPoolBuilder::new().num_threads(24).build()?;
+
+    let next_seed = Arc::new(Mutex::new(start));
+    let res_que = Arc::new(Mutex::new(BinaryHeap::new()));
+    let next_print_seed = Arc::new(Mutex::new(start));
 
     pool.install(|| {
-        (start..=end).into_par_iter().for_each(|s| {
-            let a = input[s as usize].0;
-            let b = input[s as usize].1;
-            let ans = sum(a, b);
-            println!("seed: {}, a: {}, b: {}, ans: {}", s, a, b, ans);
+        (start..=end).into_par_iter().for_each(|_| {
+            let seed = {
+                let mut next_seed = next_seed.lock().unwrap();
+                let seed = *next_seed as usize;
+                *next_seed += 1;
+                seed
+            };
+            let res = single_exec(seed, "command").unwrap();
+            let mut res_que = res_que.lock().unwrap();
+            let mut next_print_seed = next_print_seed.lock().unwrap();
+            res_que.push(Reverse(res));
+            while res_que.len() > 0 && res_que.peek().unwrap().0.seed == *next_print_seed as usize {
+                let res = res_que.pop().unwrap().0;
+                println!("{}", res);
+                *next_print_seed += 1;
+            }
         });
     });
 
     Ok(())
 }
 
+#[derive(Debug, PartialOrd, PartialEq, Eq, Ord)]
+struct ExecResult {
+    seed: usize,
+    score: String,
+    relative: Option<String>,
+    data: Vec<(String, String)>,
+}
+impl fmt::Display for ExecResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}: {:>09}{} {}",
+            format!("{:>04}", self.seed).green(),
+            self.score,
+            if let Some(relative) = self.relative.as_ref() {
+                let relative = relative.parse::<f64>().unwrap();
+                if relative < 50. {
+                    format!(" {:>6.2}", relative).red()
+                } else if relative < 80. {
+                    format!(" {:>6.2}", relative).yellow()
+                } else if relative < 95. {
+                    format!(" {:>6.2}", relative).green()
+                } else {
+                    format!(" {:>6.2}", relative).blue()
+                }
+            } else {
+                "".to_string().white()
+            },
+            self.data
+                .iter()
+                .map(|(k, v)| format!("{}={}", k, v))
+                .collect::<Vec<_>>()
+                .join(" ")
+        )
+    }
+}
+
+fn single_exec(seed: usize, command: &str) -> Result<ExecResult> {
+    let start_time = get_time();
+    let end_time = start_time + rnd::nextf() * 2.0;
+    while get_time() < end_time {}
+    let res = ExecResult {
+        seed,
+        score: rnd::get(1000000).to_string(),
+        relative: Some((rnd::nextf() * 100.0).to_string()),
+        data: vec![
+            ("a".to_string(), rnd::next().to_string()),
+            ("b".to_string(), "200".to_string()),
+            ("sum".to_string(), "16.5".to_string()),
+        ],
+    };
+    Ok(res)
+}
+
 fn sum(a: usize, b: usize) -> usize {
     let start_time = get_time();
-    while get_time() - start_time < 1.0 {}
+    let end_time = start_time + rnd::nextf() * 5.0;
+    while get_time() < end_time {}
     a + b
 }
 
